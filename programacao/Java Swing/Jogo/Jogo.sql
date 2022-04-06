bb323a302e3bb74a102f72c86a228e17de257e62
julin144 <67426830+julin144@users.noreply.github.com>
2022-04-05 21:02:57 -0300

    subir null no bd

--- programacao/Java Swing/Jogo/Jogo.sql
+++ programacao/Java Swing/Jogo/Jogo.sql
@@ -1,11 +1,11 @@
 create table personagem(
                     id_Personagem integer primary key AUTO_INCREMENT , 
-                    ds_login varchar(20),
-                    ds_senha varchar(20),
-                    nm_personagem varchar(20),
-                    nr_energia integer,
-                    nr_fome integer,
-                    nr_sono integer
+                    ds_login varchar(20) not null ,
+                    ds_senha varchar(20) not null,
+                    nm_personagem varchar(20) not null,
+                    nr_energia integer not null,
+                    nr_fome integer not null,
+                    nr_sono integernot not null
                     );
 insert into personagem(ds_login,ds_senha,nm_personagem,nr_energia,nr_fome,nr_sono ) values("jogador",123,"Julio",10,0,0);
 insert into personagem(ds_login,ds_senha,nm_personagem,nr_energia,nr_fome,nr_sono ) values("jogador2",321,"Julia",10,0,0);
